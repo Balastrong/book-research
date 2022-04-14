c70bb08bacf82d415fe305346721b4be64e96f1f
Leonardo Montini <lion.48m@gmail.com>
2022-04-14 22:25:00 +0200

    Visualizzate card con i libri restituiti dall'api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { IFormBuilder, IFormGroup } from '@rxweb/types';
 import { BookRequestModel } from './models/books/request/searchModel';
+import { Book } from './models/books/response/book';
 import { BooksService } from './services/books.service';
 import { removeEmptyFields } from './utils/dataUtils';
 
@@ -13,6 +14,7 @@ import { removeEmptyFields } from './utils/dataUtils';
 export class AppComponent implements OnInit {
   form!: IFormGroup<BookRequestModel>;
   formBuilder: IFormBuilder;
+  books: Book[] = [];
 
   constructor(formBuilder: FormBuilder, private booksService: BooksService) {
     this.formBuilder = formBuilder;
@@ -28,7 +30,11 @@ export class AppComponent implements OnInit {
 
   search(): void {
     if (this.form.value != null) {
-      this.booksService.getBooks(removeEmptyFields(this.form.value)).subscribe((res) => console.log(res));
+      this.booksService.getBooks(removeEmptyFields(this.form.value)).subscribe((res) => {
+        if (res != null) {
+          this.books = res.results || [];
+        }
+      });
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BooksService } from './services/books.service';
+import { BookCardComponent } from './components/book-card/book-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookCardComponent],
   imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [BooksService],
   bootstrap: [AppComponent],
