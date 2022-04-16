0c10ba41d1a6e1ab7e5f9b9c62159808c9be3819
Leonardo Montini <lion.48m@gmail.com>
2022-04-16 11:37:37 +0200

    Refactor e riorganizzazione componenti in cartelle
    Separazione tra componenti di dominio e generici

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { BookCardComponent } from './components/book-card/book-card.component';
-import { DialogComponent } from './components/dialog/dialog.component';
-import { ReviewsCardComponent } from './components/reviews-card/reviews-card.component';
 import { TooltipDirective } from './directives/tooltip.directive';
+import { BookCardComponent } from './domain-components/book-card/book-card.component';
+import { ReviewsCardComponent } from './domain-components/reviews-card/reviews-card.component';
+import { DialogComponent } from './generic-components/dialog/dialog.component';
 import { BooksService } from './services/books.service';
+import { TabsComponent } from './generic-components/tabs/tabs.component';
 
 @NgModule({
-  declarations: [AppComponent, BookCardComponent, TooltipDirective, DialogComponent, ReviewsCardComponent],
+  declarations: [AppComponent, TooltipDirective, BookCardComponent, ReviewsCardComponent, DialogComponent, TabsComponent],
   imports: [BrowserModule, ReactiveFormsModule, HttpClientModule, BrowserAnimationsModule],
   providers: [BooksService],
   bootstrap: [AppComponent],
