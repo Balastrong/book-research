97338ef65e8e23466e890e89972baa3243603383
Leonardo Montini <lion.48m@gmail.com>
2022-04-18 11:39:15 +0200

    Aggiunto contatore dei preferiti nella tab in homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FavouritesService } from './services/favourites.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public favouriteService: FavouritesService) {}
+}
